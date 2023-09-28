b39c1a233b67c5880b1e3fe9d84da464c7907677
akshay <akshay9897113263@gmail.com>
2023-09-28 20:28:10 +0530

    first task completed

---
+++ Task 1/button.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('#button');
+let para = document.querySelector('#writehere');
+
+btn.addEventListener('click' , ()=>{
+  para.innerText = "The button is clicked"
+})
+
